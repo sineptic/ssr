580af3746fcd485ce231cf57513be24115bfe198
sineptic <sineptic0@gmail.com>
2024-10-20 10:19:21 +0300

    feat: add action to create task without interaction

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ use ratatui::{
 use ratatui_inputs::ResultKind;
 use s_text_input_f_parser::CorrectBlocks;
 use ssr_core::tasks_facade::TasksFacade;
-use std::io::{stdout, Write};
+use std::{
+    io::{stdout, Write},
+    process::ExitCode,
+};
 
 type Task = ssr_algorithms::super_memory_2::WriteAnswer;
 type Facade<'a> = ssr_facade::Facade<'a, Task>;
@@ -46,9 +49,20 @@ fn create_task(terminal: &mut Terminal<impl Backend>) -> Result<Option<CorrectBl
     })?)
 }
 
-fn main() -> Result<()> {
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout())).unwrap();
-    let alt = alternate_screen_wrapper::AlternateScreen::enter()?.bracketed_paste()?;
+use clap::{Parser, Subcommand};
+#[derive(Parser, Debug)]
+struct Args {
+    #[command(subcommand)]
+    action: Option<Action>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Action {
+    Add { content: String },
+}
+
+fn main() -> Result<ExitCode> {
+    let args = Args::parse();
 
     let path = "storage.json";
     let file = std::fs::read_to_string(path);
@@ -57,6 +71,47 @@ fn main() -> Result<()> {
     } else {
         Facade::new("test_name".into())
     };
+
+    let success = if let Some(action) = args.action {
+        match action {
+            Action::Add { content } => {
+                let a = s_text_input_f_parser::parse_blocks(&content);
+                match a {
+                    Ok(blocks) => {
+                        if blocks.answer.iter().map(|x| x.len()).sum::<usize>() == 0 {
+                            eprintln!("Task must contain interactive elements.");
+                            false
+                        } else {
+                            let task = Task::new(blocks.blocks, blocks.answer);
+                            storage.insert(task);
+                            true
+                        }
+                    }
+                    Err(errs) => {
+                        for err in errs {
+                            eprintln!("Parsing error: {err}.");
+                        }
+                        false
+                    }
+                }
+            }
+        }
+    } else {
+        application(&mut storage)?;
+        true
+    };
+
+    save(path, storage)?;
+
+    if success {
+        Ok(ExitCode::SUCCESS)
+    } else {
+        Ok(ExitCode::FAILURE)
+    }
+}
+fn application(storage: &mut Facade) -> Result<()> {
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout())).unwrap();
+    let alt = alternate_screen_wrapper::AlternateScreen::enter()?.bracketed_paste()?;
 
     loop {
         let submenu = {
@@ -92,16 +147,7 @@ fn main() -> Result<()> {
         };
         match submenu {
             Submenu::CompleteTask => {
-                let _ = storage.complete_task(&mut |blocks| {
-                    let (_result_kind, answer) = ratatui_inputs::get_input(blocks, &mut |text| {
-                        terminal
-                            .draw(|f| f.render_widget(text, f.area()))
-                            .map(|_| ())
-                    })
-                    .transpose()?
-                    .unwrap_or((ResultKind::Ok, vec![vec![]]));
-                    Ok(answer)
-                });
+                complete_task(storage, &mut terminal);
             }
             Submenu::CreateTask => {
                 let correct_blocks = create_task(&mut terminal)?;
@@ -112,10 +158,24 @@ fn main() -> Result<()> {
             }
         }
     }
-
     drop(alt);
-    save(path, storage)?;
     Ok(())
+}
+
+fn complete_task(
+    storage: &mut ssr_facade::Facade<'_, ssr_algorithms::super_memory_2::WriteAnswer>,
+    terminal: &mut Terminal<CrosstermBackend<std::io::Stdout>>,
+) {
+    let _ = storage.complete_task(&mut |blocks| {
+        let (_result_kind, answer) = ratatui_inputs::get_input(blocks, &mut |text| {
+            terminal
+                .draw(|f| f.render_widget(text, f.area()))
+                .map(|_| ())
+        })
+        .transpose()?
+        .unwrap_or((ResultKind::Ok, vec![vec![]]));
+        Ok(answer)
+    });
 }
 
 // FIXME: first create file, than rename it to `path` to not corrupt data
